8d1634b052b88051d826c92d44969bb2e3709d40
Jeremy D. Jones <j.jones1232@gmail.com>
2021-08-10 16:19:00 -0600

    Initial commit.

---
+++ Time_Tracker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gtk;
+using System.Threading;
+
+namespace Time_Tracker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting up");
+            Task test_task = new Task();
+            Console.WriteLine("Cumulative: {0:g}", test_task.GetTime());
+            test_task.StartTimer();
+            Thread.Sleep(800);
+            test_task.StopTimer();
+            Console.WriteLine("New Cumulative: {0:g}", test_task.GetTime());
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+
+        }
+    }
+}
---
+++ Time_Tracker/Task.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Time_Tracker
+{
+    public class Task
+    {
+
+        public Task Parent { get; set; }
+
+        private TimeSpan _totalTime;
+        private readonly Stopwatch _timer;
+
+        public TimeSpan GetTime()
+        {
+            TimeSpan elapsedTime = new TimeSpan();
+
+            if (_timer.IsRunning)
+            {
+                elapsedTime = _totalTime + _timer.Elapsed;
+            }
+            else
+            {
+                elapsedTime = _totalTime;
+            }
+
+            return elapsedTime;
+        }
+
+        public void StartTimer()
+        {
+            _timer.Start();
+        }
+
+        public void StopTimer()
+        {
+            _timer.Stop();
+            _totalTime += _timer.Elapsed;
+        }
+
+        public Task(Task parent = null)
+        {
+            Parent = parent;
+            _totalTime = new TimeSpan();
+            _timer = new Stopwatch();
+            _children = new List<Task>(0);
+        }
+
+        private List<Task> _children;
+
+    }
+}
